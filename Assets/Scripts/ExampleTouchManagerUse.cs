930633f19c170919429fef2275dd205f5480aaf2
Jorrit <Jorritslaats@gmail.com>
2019-05-13 10:32:36 +0200

    Create touch manager and example script

---
+++ Assets/Scripts/ExampleTouchManagerUse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleTouchManagerUse : MonoBehaviour
+{
+    void Update()
+    {
+        //Option 1
+        Vector3 pos = TouchManager.Instance.GetTouchPosition();
+        if (pos != Vector3.zero)
+        {
+            transform.position = pos;
+        }
+
+        //Option 2
+        if (Input.touchCount > 0 )
+        {            
+            transform.position = TouchManager.Instance.GetTouchPosition();
+        }
+    }
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchManager : MonoBehaviour
+{
+    public static TouchManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Vector3 GetTouchPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector3 pos = new Vector3((Input.GetTouch(0).position.x - 960) / 100, (Input.GetTouch(0).position.y - 540) / 100, 0);
+            return pos;
+        }
+        return Vector3.zero;
+    }
+}
