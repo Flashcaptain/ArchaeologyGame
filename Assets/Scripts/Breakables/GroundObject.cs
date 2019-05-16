49cd501c500dc382a42ddaa4d06fcf49d1d8ede6
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2019-05-16 10:18:02 +0200

    Add generate map function

--- Assets/Scripts/Breakables/GroundObject.cs
+++ Assets/Scripts/Breakables/GroundObject.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class GroundObject : Breakable
 {
+    [SerializeField]
+    private bool _isGrass;
+
     public override void RemoveDurability(int damage, Tools _equippedTools)
     {
-        if (_equippedTools == Tools.Brush || _equippedTools == Tools.Shovel)
+        if ((_equippedTools == Tools.Brush && !_isGrass) || _equippedTools == Tools.Shovel)
         {
             base.RemoveDurability(damage, _equippedTools);
         }
---
+++ Assets/Scripts/MapFiller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFiller : MonoBehaviour
+{
+    [SerializeField]
+    private int _sizeX;
+
+    [SerializeField]
+    private int _sizeY;
+
+    [SerializeField]
+    private List<GroundObject> _grass;
+
+    [SerializeField]
+    private List<GroundObject> _dirt;
+
+    private void Start()
+    {
+        SpawnGrass();
+        SpawnDirt();
+    }
+
+    private void SpawnGrass()
+    {
+        int spawnSizeX = _sizeX - 1;
+        int spawnSizeY = _sizeY - 1;
+        Vector2 centerPos = new Vector2(Random.Range(-spawnSizeX, spawnSizeX), Random.Range(-spawnSizeY, spawnSizeY));
+
+        for (int x = -_sizeX; x <= _sizeX; x++)
+        {
+            for (int y = -_sizeY; y <= _sizeY; y++)
+            {
+                float floatDistance = Vector2.Distance(centerPos, new Vector2(x, y));
+                floatDistance = floatDistance / 2;
+                int intDistance = Mathf.RoundToInt(floatDistance);
+
+                if (intDistance >= _grass.Count)
+                {
+                    intDistance = _grass.Count - 1;
+                }
+                GroundObject grass = Instantiate(_grass[intDistance], new Vector3(x, y, 0), transform.rotation = Quaternion.Euler(0, 0, Random.RandomRange(0, 360)));
+            }
+        }
+    }
+
+    private void SpawnDirt()
+    {
+        for (int x = -_sizeX; x <= _sizeX; x++)
+        {
+            for (int y = -_sizeY; y <= _sizeY; y++)
+            {
+                Instantiate(_dirt[Random.Range(0,_dirt.Count)], new Vector3(x, y, 1), transform.rotation = Quaternion.Euler(0, 0, Random.RandomRange(0, 360)));
+            }
+        }
+    }
+}
