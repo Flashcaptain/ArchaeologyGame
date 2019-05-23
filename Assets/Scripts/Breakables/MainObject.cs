bdc413e1c6bb237ed093f07ac3d7f5b2f9f27b35
Jorrit <Jorritslaats@gmail.com>
2019-05-23 11:05:51 +0200

    Add camera shake as feedback

--- Assets/Scripts/Breakables/MainObject.cs
+++ Assets/Scripts/Breakables/MainObject.cs
@@ -20,6 +20,14 @@ public class MainObject : Breakable
     [SerializeField]
     private Sprite _destroySprite;
 
+    [Range(0, 99)]
+    [SerializeField]
+    private float _camShakeIntensity = 55;
+
+    [Range(0, 1)]
+    [SerializeField]
+    private float _camShakeDuration = 0.2f;
+
     private string _name;
     private int _outlines = 0;
 
@@ -28,6 +36,9 @@ public class MainObject : Breakable
         if (_equippedTools == Tools.Trowel || _equippedTools == Tools.Shovel)
         {
             base.RemoveDurability(damage, _equippedTools);
+
+            StartCoroutine(Shake(Camera.main.gameObject, _camShakeDuration, damage));
+
             _slider.maxValue = _durability;
             _slider.value = _durability - _currentDurability;
             if (_currentDurability != 0)
@@ -62,5 +73,28 @@ public class MainObject : Breakable
         float val = (float)_currentDurability / (float)_durability * 100f;
         int intVal = Mathf.RoundToInt(val);
         return intVal;
+    }
+
+    private IEnumerator Shake(GameObject camera, float duration, int damage)
+    {
+        Vector3 originalPos = camera.transform.localPosition;
+
+        float elapsed = 0.0f;
+
+            Handheld.Vibrate();
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * ((float)damage / (100 - _camShakeIntensity));
+            float y = Random.Range(-1f, 1f) * ((float)damage / (100 - _camShakeIntensity));
+
+            camera.transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        camera.transform.localPosition = originalPos;
     }
 }
