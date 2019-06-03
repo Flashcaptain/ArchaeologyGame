56dc34b9d2cf862270a5a5b6db6ff46688b36614
Jorrit <Jorritslaats@gmail.com>
2019-06-03 10:08:11 +0200

    Add back buttons to catalog and map scene

---
+++ Assets/Scripts/SceneSwitchButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchButton : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
