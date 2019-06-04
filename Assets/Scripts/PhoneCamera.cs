9c656de1bc8d4f23911a1600894abd71e8f9f952
Lex Roovers <>
2019-06-04 12:13:19 +0200

    Add camera

---
+++ Assets/Scripts/PhoneCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneCamera : MonoBehaviour
+{
+    private static WebCamTexture _phoneCamera;
+    public Texture _texture;
+
+    // Update is called once per frame
+    void Start()
+    {
+        _phoneCamera = new WebCamTexture();
+        GetComponent<Renderer>().material.mainTexture = _phoneCamera;
+
+        _phoneCamera.Play();
+        Invoke("Pause", 1.5f);
+    }
+
+    private void Pause()
+    {
+        _phoneCamera.Pause();
+        _texture = GetComponent<Renderer>().material.mainTexture;
+    }
+}
