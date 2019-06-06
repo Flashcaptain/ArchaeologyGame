76ede18376b1e681b01712171492f80779f09b4b
Jorrit <Jorritslaats@gmail.com>
2019-06-06 10:12:10 +0200

    Allow player to view catalog info

---
+++ Assets/Scripts/CatalogManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatalogManager : MonoBehaviour
+{
+    public static CatalogManager Instance;
+
+    [SerializeField]
+    private Image _objectImage;
+
+    [SerializeField]
+    private Text _title;
+
+    [SerializeField]
+    private Text _entry;
+
+    public void FillCatalogEntry(CatalogObject catalogObject)
+    {
+        _objectImage.sprite = catalogObject._sprite;
+        _title.text = catalogObject._objectName;
+        _entry.text = catalogObject._catalogEntry;
+    }
+}
--- Assets/Scripts/CatalogObject.cs
+++ Assets/Scripts/CatalogObject.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CatalogObject : LockableObject
+public class CatalogObject : MonoBehaviour
 {
-    protected override void Start()
+    public Sprite _sprite;
+
+    public string _objectName;
+
+    public string _catalogEntry;
+
+    [SerializeField]
+    private int _index;
+
+    protected void Start()
     {
-        base.Start();
+        Map thisMap = UnlockManager.Instance.GetMap(_index);
+        CheckLock(thisMap);
     }
 
-    protected override void CheckLock(Map thisMap, Text text, Image img)
+    protected void CheckLock(Map thisMap)
     {
-        base.CheckLock(thisMap, text, img);
-
         if (!thisMap._completed)
         {
-            img.sprite = _lockedSprite;
-            text.text = "Locked";
-        }
-        else
-        {
-            img.sprite = _unlockedSprite;
-            text.text = thisMap._objectName;
+            gameObject.SetActive(false);
         }
     }
 }
